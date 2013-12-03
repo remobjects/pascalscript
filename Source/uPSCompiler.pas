94c5198fd17f775672af0cd959adf46ff89bee1f
Carlo Kok <ck@carlo-kok.com>
2013-12-03 12:42:38 +0100

    0: Cast to IPointer instead of Cardinal for 64bits Pascal SCript

--- Source/uPSCompiler.pas
+++ Source/uPSCompiler.pas
@@ -14568,7 +14568,7 @@ begin
       C := CurrClass.FClassItems[I];
       if (c is TPSDelphiClassItemConstructor) and (C.NameHash = H) and (C.Name = Name) then
       begin
-        Index := Cardinal(C);
+        Index := IPointer(C);
         Result := True;
         exit;
       end;

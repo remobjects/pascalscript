8f304011aa7ec2cf7a0a90132df0919a6ccea9e2
evgenyk <evgenyk@remobjects.com>
2013-12-26 18:21:48 +0200

    {$I PascalScript.inc} was removed

--- Addon/RemObjects SDK/PascalScript_RO_Reg.pas
+++ Addon/RemObjects SDK/PascalScript_RO_Reg.pas
@@ -12,8 +12,6 @@ unit PascalScript_RO_Reg;
 { which can be obtained at http://www.remobjects.com.
 {----------------------------------------------------------------------------}
 
-{$I PascalScript.inc}
-
 interface
 
 {$R PascalScript_RO_Glyphs.res}

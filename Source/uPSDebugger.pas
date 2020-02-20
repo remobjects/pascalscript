45b18b02595ab595a44555a57c5e53d225f8b8f0
Martijn Laan <martijnlaan@gmail.com>
2020-02-20 15:24:37 +0100

    Fix for #207 which commented this line with no apparent reason + cleanup.

--- Source/uPSDebugger.pas
+++ Source/uPSDebugger.pas
@@ -531,7 +531,8 @@ function TPSCustomDebugExec.GetCallStack(var Count: Cardinal): tbtString;
       else
         Result:= Result + ParamList.Items[I] + ': ' +
                  PSVariantToString(NewTPSVariantIFC(FStack[Cardinal(Longint(StackBase) - Longint(I) - 1)], False), '') + '; ';
- //   Result := tbtString(String(Result).Remove(Length(Result)-2));
+    if Result <> '' then
+      Result := tbtString(String(Result).Remove(Length(Result)-2));
   end;
 
 var

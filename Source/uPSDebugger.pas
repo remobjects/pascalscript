922338afa084a78b715a4e560e0deafc86f6ae51
evgeny-k <git@evgenysoft.com>
2022-08-04 23:15:39 +0300

    0: issue with compiling in delphi7

--- Source/uPSDebugger.pas
+++ Source/uPSDebugger.pas
@@ -532,7 +532,7 @@ function TPSCustomDebugExec.GetCallStack(var Count: Cardinal): tbtString;
         Result:= Result + ParamList.Items[I] + ': ' +
                  PSVariantToString(NewTPSVariantIFC(FStack[Cardinal(Longint(StackBase) - Longint(I) - 1)], False), '') + '; ';
     if Result <> '' then
-      Result := tbtString(String(Result).Remove(Length(Result)-2));
+      SetLength(Result, Length(Result)-2);
   end;
 
 var

345a694d876ac84b38d1c10a8cc8adc6ffcd1525
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-06-11 20:55:44 +0200

    Cherry pick into stable: 'Fix' warning on Delphi 12.3 (and probably on 12.2 as well).

--- Source/uPSRuntime.pas
+++ Source/uPSRuntime.pas
@@ -852,10 +852,14 @@ type
     procedure SetCurrency(ItemNo: Longint; const Data: Currency);
     procedure SetBool(ItemNo: Longint; const Data: Boolean);
     procedure SetClass(ItemNo: Longint; const Data: TObject);
-
+{$IFDEF DELPHI29UP}{$WARN OVERLOADING_ARRAY_PROPERTY OFF}{$ENDIF}
+{ ^ TPSStack has *two* Items properties: one indexed with a Cardinal (defined by TPSList) and the
+    one below indexed with a Longint. Delphi 12.3 or 12.2 introduced a warning about this. Disable
+    this warning since fixing the issue breaks callers: the return types of the two properties are
+    different (Pointer vs PPSVariant). }
     property Items[I: Longint]: PPSVariant read GetItem; default;
   end;
-
+{$IFDEF DELPHI29UP}{$WARN OVERLOADING_ARRAY_PROPERTY ON}{$ENDIF}
 
 function PSErrorToString(x: TPSError; const Param: tbtstring): tbtstring;
 function TIFErrorToString(x: TPSError; const Param: tbtstring): tbtstring;
